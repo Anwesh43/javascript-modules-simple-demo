bc29eee30dd17a26c06c7c65138b932931b58158
Anwesh43 <anweshthecool0@gmail.com>
2018-05-11 03:56:12 +0530

    first attempt for module scripts

---
+++ main.mjs
@@ -0,0 +1,6 @@
+import {add, sub, div, mul} from './math.mjs'
+
+alert(`addition of 10 and 20 is ${add(10, 20)}`)
+alert(`subtraction of 10 from 20 is ${sub(20, 10)}`)
+alert(`division of 20 with 10 is ${add(20, 10)}`)
+alert(`multiplication of 10 and 20 is ${mul(10, 20)}`)
